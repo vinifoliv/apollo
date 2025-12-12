320cd3aa9a7b80118131945a404d748696576441
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2025-12-12 00:52:40 -0300

    feat: adapt shared module to clean architecture

--- src/apollo/app.py
+++ src/apollo/app.py
@@ -4,7 +4,7 @@ from apollo.peripheral_nervous_system.peripheral_nervous_system import (
 )
 from apollo.prefrontal_cortex.entities.prefrontal_cortex import PrefrontalCortex
 from apollo.prefrontal_cortex.adapters.llama_embedded_model import LlamaEmbeddedModel
-from apollo.shared.loguru_logger import LoguruLogger
+from apollo.shared.adapters.loguru_logger import LoguruLogger
 from apollo.thalamus.thalamus import Thalamus
 
 logger = LoguruLogger()
